1a45448493e6f3d2fd0a100899829b6033346e0a
Raaulsthub <rsteinmetz@inf.ufsm.br>
2022-11-16 08:49:31 -0300

    tabela de processos prototipo

---
+++ t1/tabela_processos.h
@@ -0,0 +1,13 @@
+#ifndef TABELA_H
+#define TABELA_H
+
+typedef struct processo_l processo_l;
+typedef struct tabela_t tabela_t ;
+
+tabela_t* tabela_cria();
+void tabela_destroi(tabela_t* self);
+void processo_cadastra(tabela_t* self, cpu_estado_t* cpu_estado, mem_t* mem, int pid,
+                        int proc_estado, int motiv_block);
+void proc_destroi(processo_l* self);
+
+#endif // TABELA_H
